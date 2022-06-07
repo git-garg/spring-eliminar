cac839d087eaa19fd94e7a7d5732b7da392c89f6
git-garg <103805971+git-garg@users.noreply.github.com>
2022-06-07 15:38:39 -0500

    Carga inicial de proyecto base
    Se creó el proyecto y se subió directo sin ninguna modificación

---
+++ src/main/java/ec/com/empresa/app/SpringEliminarApplication.java
@@ -0,0 +1,13 @@
+package ec.com.empresa.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringEliminarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringEliminarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ec/com/empresa/app/SpringEliminarApplicationTests.java
@@ -0,0 +1,13 @@
+package ec.com.empresa.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringEliminarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
